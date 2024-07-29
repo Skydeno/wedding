ca2c84447f6c6e742df375eb04ad9669e224498d
Konrad Pietralski <konrad.pietralski@gmail.com>
2024-07-29 22:07:49 +0200

    URL

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Photos = () => {
@@ -14,9 +15,20 @@ const Photos = () => {
         height={100}
         alt="qr-code"
       />
+      <h1 className={`text-3xl font-dance text-center p-6`}>
+        lub skorzystaj z{" "}
+        <Link
+          href={`https://weduploader.com/upload/eZskdiROAIORnbpf`}
+          className="underline text-blue"
+        >
+          tego
+        </Link>{" "}
+        linku
+      </h1>
       <ul className="text-center mt-4">
         <li>1. Po dodaniu nie można usunąć</li>
         <li>2. Jeżeli dodałeś coś omyłkowo - daj nam znać a usuniemy</li>
+        <li>3. Po wszystkim udostępnimy tutaj link do zdjęć</li>
       </ul>
     </div>
   );
